b835f990290d3f192895c621c5990e34639b0f8f
Andrew Krause <andrew@nodeprime.com>
2014-09-03 15:48:15 -0700

    [UPDATE] Adds basic validation ability

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+(function() {
+
+  var inputBox = document.getElementById('input');
+  var resultBox = document.getElementById('result');
+  var validateButton = document.getElementById('validateButton');
+
+  $.getJSON('https://rawgit.com/doc-metrix/schema/master/metricsSchema.json')
+  .done(function ( schema ) {
+
+    var validate = function validate() {
+      var specification = JSON.parse(inputBox.value);
+      if (specification) {
+        var result = tv4.validateMultiple(specification, schema);
+        if (result.valid) {
+          resultBox.innerHTML = "VALID!";
+        } else {
+          resultBox.innerHTML = "INVALID!!";
+          console.log(result);
+        }
+      }
+    };
+    
+    validateButton.addEventListener('click', validate, false);
+  });
+
+})();
