43f42c36ecd9f9d009e9c694e4744e3c19af029e
Andrew Krause <andrew@nodeprime.com>
2014-09-03 17:04:35 -0700

    [UPDATE] Display validation errors

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,7 @@
   // HTML elements
   var inputBox = document.getElementById('input'),
       resultBox = document.getElementById('result'),
+      errorBox = document.getElementById('errors'),
       validateButton = document.getElementById('validateButton');
 
   // always grab the latest version of the schema
@@ -23,12 +24,38 @@
       if (specification) {
         result = tv4.validateMultiple(specification, schema);
         if (result.valid) {
-          resultBox.innerHTML = "VALID!";
+          resultBox.innerHTML = 'VALID!';
+          errorBox.innerHTML = '';
         } else {
-          resultBox.innerHTML = "INVALID!!";
-          console.log(result);
+          resultBox.innerHTML = 'INVALID!!';
+          errorBox.innerHTML = processErrors(result);
         }
       }
+    }
+
+    /**
+    * FUNCTION: processErrors( resultObj )
+    *   Generates an HTML representation of validation errors
+    *
+    * @param {Object} resultObj - validation results object
+    * @returns {String} HTML string
+    */
+    function processErrors( result ) {
+      var htmlString = '';
+
+      if (result.valid) {
+        return htmlString;
+      }
+
+      for (var i = 0; i < result.errors.length; i++) {
+        htmlString += '<div class="error">';
+        htmlString += '<strong>Metric:</strong> ' + result.errors[i].dataPath.substr(1) + '<br />';
+        htmlString += '<strong>Error:</strong> ' + result.errors[i].message;
+        htmlString += '</div>';
+        htmlString += '<hr />';
+      }
+
+      return htmlString;
     }
   });
 
