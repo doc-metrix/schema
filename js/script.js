1d647494ed4716d900d3e8e8ae18baf93e798d59
kgryte <kgryte@gmail.com>
2014-09-15 18:35:07 -0700

    [UPDATE] results output.

--- js/script.js
+++ js/script.js
@@ -2,11 +2,29 @@
 (function() {
 	'use strict';
 
-	var URLs = {};
+	var url,
+		schema,
+		button,
+		input,
+		results,
+		errors;
 
-	URLs.schema = 'https://api.github.com/repos/doc-metrix/schema/contents/schema/index.json';
+	url = 'https://api.github.com/repos/doc-metrix/schema/contents/schema/index.json';
 
-	getResource( URLs.schema, onSchema );
+	// Get the validation elements:
+	button = document.querySelector( '[name="validate"]' );
+	input = document.querySelector( '[name="validation-input"]' );
+	results = document.querySelector( '.results' );
+	errors = document.querySelector( '.errors' );
+
+	if ( !button || !results || !input || !errors ) {
+		throw new Error( 'Unable to find validation elements.' );
+	}
+
+	// Get the JSON schema:
+	getResource( url, onSchema );
+
+	return;
 
 	// FUNCTIONS //
 
@@ -47,8 +65,53 @@
 	* @param {String} blob - content blob which will be parsed as JSON
 	*/
 	function onSchema( blob ) {
-		var schema = JSON.parse( window.atob( JSON.parse( blob ).content ) );
-		console.log( schema );
+		schema = JSON.parse( window.atob( JSON.parse( blob ).content ) );
+		addListener();
 	} // end FUNCTION onSchema()
+
+	/**
+	* FUNCTION: addListener()
+	*	Adds an event listener to a validation button.
+	*
+	* @private
+	*/
+	function addListener() {
+		button.addEventListener( 'click', validate, false );
+	} // end FUNCTION addListener()
+
+	/**
+	* FUNCTION: validate()
+	*	Validates a metric specification and outputs the validation results.
+	*
+	* @private
+	*/
+	function validate() {
+		var spec = input.value,
+			str = '',
+			res;
+		if ( !spec ) {
+			errors.innerHTML = '<p>Validation requires a metric specification.</p>';
+			return;
+		}
+		try {
+			spec = JSON.parse( spec );
+		} catch ( error ) {
+			errors.innerHTML = '<p>A metric specification must be valid JSON.</p>';
+			return;
+		}
+		res = tv4.validateMultiple( spec, schema );
+		if ( res.valid ) {
+			results.innerHTML = '<p>Specification is valid.</p>';
+			return;
+		}
+		errs = results.errors;
+		for ( var i = 0; i < errs.length; i++ ) {
+			str += '<p class="error">';
+			str += '<strong>Metric:</strong>' + errs[ i ].dataPath.substr( 1 ) + '<br/>';
+			str += '<strong>Error:</strong>' + errs[ i ] .message;
+			str += '</p><hr>';
+		}
+		errors.innerHTML = str;
+	} // end FUNCTION validate()
 
 })();
